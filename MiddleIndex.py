2d571ea3ed19d6dfc6b2a87d50eb00145c48bb7d
Daniel <114618893+kookaba@users.noreply.github.com>
2022-12-13 14:37:51 -0600

    Created MiddleIndex.py

---
+++ MiddleIndex.py
@@ -0,0 +1,19 @@
+"""
+Given a 0-indexed integer array nums, find the leftmost middleIndex (i.e., the smallest amongst all the possible ones).
+
+A middleIndex is an index where nums[0] + nums[1] + ... + nums[middleIndex-1] == nums[middleIndex+1] + nums[middleIndex+2] + ... + nums[nums.length-1].
+
+If middleIndex == 0, the left side sum is considered to be 0. Similarly, if middleIndex == nums.length - 1, the right side sum is considered to be 0.
+
+Return the leftmost middleIndex that satisfies the condition, or -1 if there is no such index.
+"""
+
+class Solution:
+    def findMiddleIndex(self, nums: List[int]) -> int:
+        left = 0
+        total = sum(nums)
+        for i in range(len(nums)):
+            left += nums[i]
+            if left == (total - (left - nums[i])):
+                return i
+        return -1
